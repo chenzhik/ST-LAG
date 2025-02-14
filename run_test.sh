2f3970906b7e7af739a78a771d66bab53843919d
Zhikai Chen <32872030+chenzhik@users.noreply.github.com>
2025-02-14 16:57:57 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 # LR video generation
-CUDA_VISIBLE_DEVICES=0 python -m scripts.animate --exp_config configs/ablation/inference.yaml --H 2048 --W 2048 --L 32 --xformers
+CUDA_VISIBLE_DEVICES=0 python -m scripts.animate --exp_config configs/ablation/inference.yaml --H 1024 --W 1024 --L 16 --xformers
 
 # HR video generation
 CUDA_VISIBLE_DEVICES=0 python -m scripts.animate_st_grouping --exp_config configs/ablation/inference.2k32f.yaml --H 2048 --W 2048 --L 32 --xformers
